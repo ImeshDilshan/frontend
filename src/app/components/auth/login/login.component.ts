3e713c6688495803a8a37547f44a2c6283c68ac4
Imesh Dilshan <86838087+ImeshDilshan@users.noreply.github.com>
2023-05-21 17:04:39 +0530

    reasech

---
+++ src/app/components/auth/login/login.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user/user.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  public loginForm: FormGroup;
+  public registerForm: FormGroup;
+  private userName:FormControl;
+
+  constructor(private formBuilder: FormBuilder,private userService:UserService,private router: Router
+    ) {
+    this.createLoginForm();
+    this.createRegisterForm();
+  }
+
+  owlcarousel = [
+    {
+      title: "Welcome to FreshMeat.lk",
+      desc: "Mobile Application Products and Reports",
+    },
+    {
+      title: "Welcome to FreshMeat.lk",
+      desc: "Mobile Application Products and Reports",
+    },
+    {
+      title: "Welcome to FreshMeat.lk",
+      desc: "Mobile Application Products and Reports",
+    }
+  ]
+  owlcarouselOptions = {
+    loop: true,
+    items: 1,
+    dots: true
+  };
+
+  createLoginForm() {
+    this.loginForm = this.formBuilder.group({
+      userName: this.userName,
+      password: [''],
+    })
+    this.userName=new FormControl('',[Validators.required])
+
+  }
+  createRegisterForm() {
+    this.registerForm = this.formBuilder.group({
+      userName: [''],
+      password: [''],
+      confirmPassword: [''],
+    })
+  }
+
+
+  ngOnInit() {
+  }
+
+  onSubmit() {
+
+  }
+
+}
